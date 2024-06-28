6f7c80a8d6a30b52156411e8195d3f810c62b66b
Akshar Goyal <claim.akshar@gmail.com>
2024-06-28 14:09:19 +0000

    Added files

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use endoflife::request::api_request_all_rust_cycles;
+
+fn main() {
+    let _all_cycles = api_request_all_rust_cycles().unwrap();
+}
---
+++ src/rust.rs
@@ -0,0 +1,18 @@
+use serde::{ Serialize, Deserialize };
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RustSingleCycle {
+    pub release_date: Option<String>,
+    pub eol: DateOrBool,
+    pub latest: Option<String>,
+    pub latest_release_date: Option<String>,
+    pub lts: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum DateOrBool {
+    Bool(bool),
+    Date(String),
+}
